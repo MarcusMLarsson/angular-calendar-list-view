9f8df298ea378d2d5d79f42fc8cc182ee3bdf1ee
Marcus <marcus.larsson@odevo.com>
2024-10-11 13:52:22 +0200

    feat(list-view): add events as input

--- projects/angular-calendar/src/modules/list/calendar-list-view/calendar-list-view.component.ts
+++ projects/angular-calendar/src/modules/list/calendar-list-view/calendar-list-view.component.ts
@@ -1,7 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CalendarEvent } from 'calendar-utils';
 
 @Component({
   selector: 'mwl-calendar-list-view',
-  template: `<h1>Hello world</h1> `,
+  template: `{{ events | json }} `,
 })
-export class CalendarListViewComponent {}
+export class CalendarListViewComponent {
+  /**
+   * The current view date
+   */
+  @Input() viewDate: Date;
+
+  /**
+   * An array of events to display on view
+   * The schema is available here: https://github.com/mattlewis92/calendar-utils/blob/c51689985f59a271940e30bc4e2c4e1fee3fcb5c/src/calendarUtils.ts#L49-L63
+   */
+  @Input() events: CalendarEvent[] = [];
+}
